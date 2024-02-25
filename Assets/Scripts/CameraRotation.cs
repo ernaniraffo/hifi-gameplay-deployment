8cb55b594653c4f45f716771fb7c8a9e4b8b7907
ernaniraffo <ernani.raffo@icloud.com>
2024-02-25 10:16:11 -0800

    fixed smooth camera rotation

--- Assets/Scripts/CameraRotation.cs
+++ Assets/Scripts/CameraRotation.cs
@@ -4,36 +4,41 @@ using UnityEngine;
 
 public class RotateCamera : MonoBehaviour
 {
+    public GameObject otherObject;
+
     private float smoothTime = 0.3f;
+    private float turnTime = 0.3f;
 
     private bool rotatingLeft = false;
     private bool rotatingRight = false;
+
+    private bool rotating = false;
 
     // Update is called once per frame
     void Update()
     {
         // Set left rotation
-        if (!isRotating() && Input.GetKeyDown(KeyCode.Q))
-        {
-            SetLeftRotation();
-        }
+        //if (!isRotating() && Input.GetKeyDown(KeyCode.Q))
+        //{
+        //    SetLeftRotation();
+        //}
 
         // Continue rotating object left
-        if (rotatingLeft)
+        if (!isRotating() && Input.GetKeyDown(KeyCode.Q))
         {
-            RotateLeft();
+            StartCoroutine(Rotate(transform, otherObject.transform, Vector3.up, 90, turnTime));
         }
 
         // Rotate right
-        if (!isRotating() && Input.GetKeyDown(KeyCode.E))
-        {
-            SetRightRotation();
-        }
+        //if (!isRotating() && Input.GetKeyDown(KeyCode.E))
+        //{
+        //    SetRightRotation();
+        //}
 
         // Continue rotating object right
-        if (rotatingRight)
+        if (!isRotating() && Input.GetKeyDown(KeyCode.E))
         {
-            RotateRight();
+            StartCoroutine(Rotate(transform, otherObject.transform, Vector3.up * -1, 90, turnTime));
         }
     }
 
@@ -60,6 +65,9 @@ public class RotateCamera : MonoBehaviour
             transform.rotation = Quaternion.Slerp(fromAngle, toAngle, t);
             yield return null;
         }
+        float y = transform.eulerAngles.y;
+        float ceiled = Mathf.Ceil(y);
+        Debug.Log("y " + y + " ceiled " + ceiled);
         rotatingLeft = false;
     }
 
@@ -74,11 +82,38 @@ public class RotateCamera : MonoBehaviour
             transform.rotation = Quaternion.Slerp(fromAngle, toAngle, t);
             yield return null;
         }
+        float y = transform.eulerAngles.y;
+        float ceiled = Mathf.Ceil(y);
+        Debug.Log("y " + y + " ceiled " + ceiled);
         rotatingRight = false;
     }
 
     bool isRotating()
     {
-        return rotatingLeft || rotatingRight;
+        return rotating;
+    }
+
+    IEnumerator Rotate(Transform thisTransform, Transform otherTransform, Vector3 rotateAxis, float degrees, float totalTime)
+    {
+        rotating = true;
+
+        var startRotation = thisTransform.rotation;
+        var startPosition = thisTransform.position;
+        transform.RotateAround(otherTransform.position, rotateAxis, degrees);
+        var endRotation = thisTransform.rotation;
+        var endPosition = thisTransform.position;
+        thisTransform.rotation = startRotation;
+        thisTransform.position = startPosition;
+
+        var rate = degrees / totalTime;
+        for (float i = 0; i < degrees; i += Time.deltaTime * rate)
+        {
+            thisTransform.RotateAround(otherTransform.position, rotateAxis, Time.deltaTime * rate);
+            yield return null;
+        }
+
+        thisTransform.rotation = endRotation;
+        thisTransform.position = endPosition;
+        rotating = false;
     }
 }
